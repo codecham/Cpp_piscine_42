8da1c60b2809b213812a0e03ed8fa1b8c740c4d6
Corentin Dechamps <corentin@Corentins-MacBook-Pro.local>
2023-02-23 08:35:19 +0100

    05 in progress

--- Module_05/ex03/Intern.cpp
+++ Module_05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: dcorenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 19:56:47 by dcorenti          #+#    #+#             */
-/*   Updated: 2023/02/19 19:59:30 by dcorenti         ###   ########.fr       */
+/*   Updated: 2023/02/23 08:31:57 by dcorenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 
+#define TOTAL_FORMS 3
+
 Intern::Intern()
 {
 
@@ -22,7 +24,7 @@ Intern::Intern()
 
 Intern::Intern(const Intern& copy)
 {
-
+	*this = copy;
 }
 
 Intern::~Intern(void)
@@ -32,11 +34,39 @@ Intern::~Intern(void)
 
 Intern&	Intern::operator=(const Intern& copy)
 {
-
+	(void)copy;
+	return(*this);
 }
 
-Aform	*Intern::makeForm(std::string form_name, std::string target)
+AForm	*Intern::makeForm(std::string form_name, std::string target)
 {
-	
+	std::string namesArray[TOTAL_FORMS] = {"shrubbery creation", "robotomy request", "presidential pardon"};
+	AForm	*(Intern::*createForm[TOTAL_FORMS])(std::string target) = {&Intern::createShrubbery, &Intern::createRobotomy, &Intern::createPardon};
+	int i = 0;
+
+	while (i < TOTAL_FORMS)
+	{
+		if (form_name == namesArray[i])
+		{
+			std::cout << "Intern creates " << form_name << std::endl;
+			return ((this->*createForm[i])(target));
+		}
+		i++;
+	}
+	std::cout << form_name << " is not a valid form" << std::endl;
 }
 
+AForm 	*Intern::createShrubbery(std::string target)
+{
+	return (new ShrubberyCreationForm(target));
+}
+
+AForm 	*Intern::createPardon(std::string target)
+{
+	return (new PresidentialPardonForm(target));
+}
+
+AForm 	*Intern::createRobotomy(std::string target)
+{
+	return (new RobotomyRequestForm(target));
+}
--- Module_05/ex03/Intern.hpp
+++ Module_05/ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: dcorenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 19:52:42 by dcorenti          #+#    #+#             */
-/*   Updated: 2023/02/19 19:58:24 by dcorenti         ###   ########.fr       */
+/*   Updated: 2023/02/23 08:25:18 by dcorenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,12 @@ class Intern
 		Intern(const Intern& copy);
 		~Intern(void);
 		Intern& operator=(const Intern& copy);
-		Aform *makeForm(std::string form_name, std::string target);
-
+		AForm *makeForm(std::string form_name, std::string target);
+	
+	private:
+		AForm *createShrubbery(std::string target);
+		AForm *createPardon(std::string target);
+		AForm *createRobotomy(std::string target);
 };
 
 #endif
